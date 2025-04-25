9d41f3237e370a865cf33fd16c219fa04faa861c
ANNA CAROLINA GRILLO MARCIANO <25170136@educ123.sp.senai.br>
2025-04-25 07:56:40 -0300

    add packet

--- funções/moldura.js
+++ funções/moldura.js
@@ -1,1 +1,16 @@
 // DESENHA UMA MOLDURA EM FRASE
+const readline = require('readline-sync');
+const ch = '█';
+
+function barra(qtde){
+
+    for(let i=0; i<qtde+4; i++){
+        process.stdout.write(ch);
+     }
+}
+//fora da funçao
+
+const frase = readline.question(' \nDigite uma frase: ');
+barra(frase.length);
+console.log(`\n${ch} ${frase} ${ch}`);
+barra(frase.length)
